be927f2cb4ce5654117ea46b671d2fc57485bcdc
maaru <maratarzymatov288@gmail.com>
2025-02-03 06:03:54 +0600

    update | newfunc

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ rm -rf ~/.config/yazi/plugins
 rm -rf ~/.config/yazi/flavors/
 git clone https://github.com/ndtoan96/ouch.yazi.git ~/.config/yazi/plugins/ouch.yazi
 git clone https://github.com/imsi32/yatline-catppuccin.yazi.git ~/.config/yazi/plugins/yatline-catppuccin.yazi
+git clone https://github.com/imsi32/yatline-gruvbox.yazi.git ~/.config/yazi/plugins/yatline-gruvbox.yazi
 ya pack --upgrade
 sleep 2
 clear
