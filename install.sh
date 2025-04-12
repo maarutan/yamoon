b1d47d51f2f24e3b451a93fdfe25a48c9cb3d0ee
maaru <maratarzymatov288@gmail.com>
2025-04-12 17:09:04 +0600

    update | install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Цвета
+RESET='\033[0m'
+BOLD='\033[1m'
+DIM='\033[2m'
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+GRAY='\033[90m'
+
 REPO_NAME="yamoon"
 REPO="https://github.com/maarutan/$REPO_NAME"
 REPO_PATH="$HOME/$REPO_NAME"
@@ -9,35 +21,41 @@ YAZI="$HOME/.config/yazi"
 YAZI_BAKUP() {
     if [[ -d "$YAZI" ]]; then
         mv "$YAZI" "$YAZI"_"$TODAY".bak
-        echo -e "\n- you had a Yazi config"
-        echo "- backup created: $(ls "$YAZI"_"$TODAY".bak) :D"
+        echo -e "\n${YELLOW}- you had a Yazi config${RESET}"
+        sleep 0.5
+        echo -e "${GREEN}- backup created: $(echo "${YAZI}_${TODAY}.bak") :D${RESET}\n"
+        sleep 0.5
     fi
 }
 
 CHECK_REPO_HAVE() {
     if [[ ! -d "$REPO_PATH" ]]; then
         if command -v git > /dev/null; then
-            echo -e "\n~~~> $REPO_PATH"
-            echo "- repo not found :("
-            echo "- cloning repo... :D"
-            sleep 0.1
+            sleep 0.5
+            echo -e "\n${CYAN}~~~> $REPO_PATH${RESET}"
+            sleep 0.5
+            echo -e "${RED}- repo not found :(${RESET}"
+            sleep 0.5
+            echo -e "${GREEN}- cloning repo... :D${RESET}\n"
+            sleep 0.2
             git clone "$REPO" "$YAZI"
         else
             sleep 0.1
-            echo "missing dependency: git :("
+            echo -e "${RED}missing dependency: git :(${RESET}"
         fi
     else
-        sleep 0.1
-        echo -e "\n~~~> $REPO_PATH"
-        echo "- repo already exists :D"
+        echo -e "\n${CYAN}~~~> $REPO_PATH${RESET}"
+        sleep 0.5
+        echo -e "${GREEN}- repo already exists :D${RESET}\n"
+        sleep 0.5
     fi
 }
 
 START_YAMOON() {
     clear
-    echo "welcome to yamoon :D"
+    echo -e "${BOLD}welcome to yamoon :D${RESET}"
     sleep 1
-    yazi || { clear; echo "could not start yazi :( "; }
+    yazi || { clear; echo -e "${RED}could not start yazi :( ${RESET}"; }
 }
 
 IFMAIN() {
@@ -46,7 +64,7 @@ IFMAIN() {
         YAZI_BAKUP
         CHECK_REPO_HAVE
         START_YAMOON
-        echo "setup complete :D"
+        echo -e "${GREEN}setup complete :D${RESET}"
     fi
 }
 
