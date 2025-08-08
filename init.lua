cf3cfc5a16ac359b28381250638e9b6e4dd3c75f
maaru <maratarzymatov288@gmail.com>
2025-08-09 04:31:00 +0600

    edit | init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,26 @@
--- ┬┌┐┌┬┌┬┐
--- │││││ │
--- ┴┘└┘┴ ┴
+--!/usr/bin/env lua
+-- ┬ ┌┐┌ ┬ ┌┬┐
+-- │ │││ │  │
+-- ┴ ┘└┘ ┴  ┴
+--------------------------
 local plug_conf_dir = "plugcfg" -- your configure dir for plugins
 local initfiles = { -- write name file for init your plug config
 	"git",
 	"yatline",
-	"augment_command",
-	"eza_preview",
 	"simple_tag",
-	"relative_motions",
 	"full_border",
+	"eza_preview",
+	"augment_command",
+	"relative_motions",
 }
--- ┬  ┌─┐┌─┐┬┌─┐
--- │  │ ││ ┬││
--- ┴─┘└─┘└─┘┴└─┘
+
+-- ┬  ┌─┐┌─┐┌┬┐
+-- │  │ │├─┤ ││
+-- ┴─┘└─┘┴ ┴─┴┘
+---------------------------
 -- init file logic
 for _, name in ipairs(initfiles) do
 	dofile(os.getenv("HOME") .. "/.config/yazi/" .. plug_conf_dir .. "/" .. name .. ".lua")
 end
+
+return { plug_conf_dir = plug_conf_dir }
